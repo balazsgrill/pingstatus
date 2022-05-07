d9d653d800f09e2b27e28d6dda9f64a1c3431e75
balazsgrill <balazs.grill@incquerylabs.com>
2022-05-07 07:15:21 +0200

    Providing an HTTP service instead of api call

--- main.go
+++ main.go
@@ -1,58 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/go-ping/ping"
 )
 
 type target struct {
-	Host    string `json:"host"`
-	Webhook string `json:"webhook"`
-}
-
-type config struct {
-	Ping []*target `json:"ping"`
-}
-
-type payload struct {
-	Trigger string `json:"trigger"`
-	Message string `json:"message"`
-	Name    string `json:"name,omitempty"`
-	Status  string `json:"status,omitempty"`
-}
-
-func (t *target) send(p *payload) error {
-	data, err := json.Marshal(p)
-	if err != nil {
-		return err
-	}
-	resp, err := http.Post(t.Webhook, "application/json", bytes.NewReader(data))
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		resp.Body.Close()
-		fmt.Printf("Error in webhook '%s'\n", t.Webhook)
-		fmt.Printf("%s\n", data)
-		return fmt.Errorf("webhook returned HTTP %d: %s", resp.StatusCode, string(body))
-	}
-	return nil
+	Host string `json:"host"`
 }
 
 func (t *target) check() (*ping.Statistics, error) {
 	p, err := ping.NewPinger(t.Host)
+	p.Interval = time.Millisecond * 100
+	p.Timeout = time.Second * 2
 	if err != nil {
 		return nil, err
 	}
@@ -64,62 +29,45 @@ func (t *target) check() (*ping.Statistics, error) {
 	return p.Statistics(), nil
 }
 
-func (t *target) ping() {
-	s, err := t.check()
-
-	pl := &payload{
-		Name:    "ping",
-		Trigger: "up",
-		Status:  "OPERATIONAL",
-	}
-
-	if err != nil {
-		pl.Trigger = "down"
-		pl.Status = "MAJOROUTAGE"
-		pl.Message = err.Error()
-	} else {
-		pl.Message = fmt.Sprintf("Received %d/%d, avg time %d ms", s.PacketsRecv, s.PacketsSent, s.AvgRtt)
-
-		if s.PacketsRecv == 0 {
-			pl.Trigger = "down"
-			pl.Status = "MAJOROUTAGE"
-		} else if s.PacketsRecv < s.PacketsSent {
-			pl.Status = "PARTIALOUTAGE"
-		}
-	}
-
-	err = t.send(pl)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
 func main() {
-	var configfile string
+	var key string
+	var port int
 
-	flag.StringVar(&configfile, "c", "", "Configuration file")
+	flag.StringVar(&key, "k", "", "API Key")
+	flag.IntVar(&port, "p", 8080, "HTTP Port")
 	flag.Parse()
 
-	c, err := os.ReadFile(configfile)
-	if err != nil {
-		panic(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		host := r.URL.Query().Get("host")
+		log.Printf("Request on %s\n", host)
+		if key == "" {
+			log.Println("Key mismatch")
+			w.WriteHeader(401)
+			return
+		}
+		if host == "" {
+			log.Println("Host not set")
+			w.WriteHeader(404)
+			return
+		}
+		stats, err := (&target{Host: host}).check()
+		if err != nil {
+			log.Printf("Ping error %v\n", err)
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+			return
+		}
+		if stats.PacketsRecv < stats.PacketsSent {
+			answer := fmt.Sprintf("Received %d packets out of %d", stats.PacketsRecv, stats.PacketsSent)
+			log.Println(answer)
+			w.WriteHeader(503)
+			w.Write([]byte(answer))
+			return
+		}
+		w.Write([]byte("OK"))
+	})
 
-	var conf config
-	err = json.Unmarshal(c, &conf)
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		(&conf).doping()
-		time.Sleep(10 * time.Minute)
-	}
-}
-
-func (c *config) doping() {
-	for _, t := range c.Ping {
-		go t.ping()
-	}
+	// listen to port
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
